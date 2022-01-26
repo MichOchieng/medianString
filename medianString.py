e398affd04d4d658f2ffcc5b03966dd390ecb429
Mich Ochieng <michochieng@gmail.com>
2022-01-26 00:31:33 -0800

    First commit

---
+++ medianString.py
@@ -0,0 +1,85 @@
+import re     
+from itertools import permutations
+from sys       import exit
+from os        import walk
+from pathlib   import Path
+
+class medianString:
+
+    def __init__(self) -> None:
+        self.getFiles()
+
+    ALPHABET      = ['A','C','T','G']
+    DNA_LIST      = []
+    INPUT_FILES   = []
+    KMER_LIST     = []
+
+    BEST_KMER     = ""
+    BEST_KMER_D   = 0
+    K             = 0
+
+    # Finds input files from the current directory
+    def getFiles(self):
+        # Gets the path of the current directory
+        myPath = Path(__file__).parent.resolve() 
+
+        # Loop through files in the given directory 'myPath'
+        for file in next(walk(myPath), (None, None, []))[2]:
+            # Will grab all text input files that have the naming convention
+            if re.search("^input_.*.txt$",file): 
+                self.INPUT_FILES.append(file)
+ 
+    def fillKmerList(self,k):
+        for kmer in permutations(self.ALPHABET, k):
+            self.KMER_LIST.append(kmer)
+        print(len(self.KMER_LIST))
+        print(self.KMER_LIST)
+
+    def distance(self,kmer):
+        distance = 0
+
+        for x,y in zip(kmer,self.DNA_LIST):
+            if x != y:
+                distance+=1
+
+        return distance
+        
+
+    def median(self,myFile):
+        # Refresh class lists for each incoming file
+        self.DNA_LIST.clear() 
+        self.KMER_LIST.clear() 
+
+        # Fill the DNA_LIST class variable with DNA from a given input file
+        try: 
+            with open(myFile,"r",encoding='utf-8',errors='ignore') as file:
+                lines = file.readlines()
+
+                # Splits up the DNA from the input file into an array then loops over it saving each strand to the class variable DNA_LIST
+                for dna in re.split('[\s]',lines[1]):
+                    self.DNA_LIST.append(dna)
+        except OSError:
+            print("Could not open file " + myFile + ".")
+            exit()
+
+        for kmer in self.KMER_LIST:
+            if self.distance(kmer) < self.BEST_KMER_D:
+                self.BEST_KMER = kmer
+                self.BEST_KMER_D = self.distance(kmer)
+
+        return self.BEST_KMER
+        # for each kmer of size k
+        # if distance(kmer,dna) < d(bestKmer,dna):
+        #   bestKmer = kmer
+        #   return(bestKmer)
+
+
+    def run(self):
+        for file in self.INPUT_FILES:
+            print(file + " produced a best-k-mer of")
+            print(self.median(file))
+
+
+program = medianString()
+program.fillKmerList(3)
+#program.run()
